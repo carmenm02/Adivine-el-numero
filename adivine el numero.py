b80c8c4e9a0a3853b1cda498370bee00e759cab4
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2021-11-04 11:02:19 +0100

    corregido

--- adivine el numero.py
+++ adivine el numero.py
@@ -1,15 +1,15 @@
 import random 
 numero = random.randint(0, 100)
-print(numero)
-intento = int(input("Adivina el numero"))
+intento = int(input("Introduzca el numero: "))
 numero_intentos = 1
 while intento != numero :
+    numero_intentos += 1
+    intento = int(input("Introduzca el numero: "))
     if intento <= numero :
         print("Demasiado pequeño")
-        numero_intentos += 1
     elif numero >= numero :
         print("Demasiado grande")
-        numero_intentos += 1
 if intento == numero :
     print("¡Has ganado!")
+
 print("Cantidad de intentos: " + str(numero_intentos))
